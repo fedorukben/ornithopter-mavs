b55c7da9594127696190c3a5f2f078378f07c0cb
nshah271 <104401783+nshah271@users.noreply.github.com>
2022-05-02 11:06:47 -0400

    Update HeatMap.py

--- HeatMap.py
+++ HeatMap.py
@@ -8,7 +8,11 @@ import pandas as pd
 df = pd.read_csv(r'D:\NSII\FSI-MAVs\HeatMapTry.csv')
 df = df.pivot("Birds", "Winglength", "ReynoldsNumber")
     
-fig = px.imshow(df,color_continuous_scale = px.colors.sequential.thermal,title = "Title can be decided soon")
+fig = px.imshow(df,color_continuous_scale = px.colors.sequential.Plasma,title = "Winglength v/s Reynolds Number")
 fig.update_layout(title_font = {'size':27}, title_x = 0.5)
-fig.update_traces(hoverongaps = False)
+
+fig.update_traces(hoverongaps = False, 
+                 hovertemplate = "Winglength: %{y}"
+                                 "<br> Winglength: %{x}"
+                                 "<br> Reynolds Number: %{z}<extra></extra>")
 fig.show()
